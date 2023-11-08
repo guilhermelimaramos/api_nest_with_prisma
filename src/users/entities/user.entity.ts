5df7930fc674fde0c5f8f73f3ef1c08994414a0a
Guilherme Lima <glimaramos013@gmail.com>
2023-11-08 10:39:55 -0300

    create user entity and user dto

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line prettier/prettier
+import { User } from "@prisma/client";
+
+export class UserEntity implements User {
+  id: number;
+  email: string;
+  name: string;
+  admin: boolean;
+  createdAt: Date;
+}
