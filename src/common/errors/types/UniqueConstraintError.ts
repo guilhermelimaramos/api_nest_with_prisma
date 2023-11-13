cb58e8ac9738999829867dc9d6be17bad6d62c4d
Guilherme Lima <glimaramos013@gmail.com>
2023-11-13 09:20:49 -0300

    Unique Constraint Error

---
+++ src/common/errors/types/UniqueConstraintError.ts
@@ -0,0 +1,10 @@
+import { ConflictError } from './ConflictError';
+import { PrismaClientError } from './PrismaClientError';
+
+export class UniqueConstraintError extends ConflictError {
+  constructor(e: PrismaClientError) {
+    const uniqueField = e.meta.taget;
+
+    super(`A record with this ${uniqueField} already exist`);
+  }
+}
