baa5a309889c7750e5c944a3fc779e567577eb4b
Guilherme Lima <glimaramos013@gmail.com>
2023-11-14 11:02:45 -0300

    Post service and controller

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsRepository } from './repositories/post.repository';
+import { PostEntity } from './entities/post.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly repository: PostsRepository) {}
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.repository.create(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number): Promise<PostEntity> {
+    const post = await this.repository.findOne(id);
+
+    if (!post) throw new NotFoundError('Post not found');
+
+    return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    return this.repository.update(id, updatePostDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return this.repository.remove(id);
   }
 }
