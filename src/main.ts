785c21c856bc4483834c27e7099796a66d42a8af
Guilherme Lima <glimaramos013@gmail.com>
2023-11-30 10:06:58 -0300

    Swagger config

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,21 @@ import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthoriz
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { HttpExceptionsFilterTsFilter } from './common/filters/http-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The simple blog API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, IsBoolean, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'User email' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Username' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Define if the user is an administrator',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
